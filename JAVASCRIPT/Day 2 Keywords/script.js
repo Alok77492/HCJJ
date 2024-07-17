8ef5f5a1ec673e6dd519d8170023e470a16acb21
alokks11216 <alokks11216@gmail.com>
2024-07-17 15:48:24 +0530

    HCJ

---
+++ JAVASCRIPT/Day 2 Keywords/script.js
@@ -0,0 +1,113 @@
+// let a=5;
+// a+=5
+// console.log(a)
+
+// let a=10;
+// a-=5
+// console.log(a)
+
+
+// let a=2
+// a*=2
+// a=a*2
+// console.log(a)
+
+
+// let a=10
+// a/=2
+// // a=a/2
+// console.log(a)
+
+// let a=2
+// a**=2
+// // a=a**2
+// console.log(a)
+
+
+/*let name=prompt('Enter name')
+
+if(name=="puneet"){
+    console.log("Ab tu dekh...")
+}
+else if(name=="carry"){
+    console.log("Asambhav...")
+}
+else if(name=="harsh"){
+    console.log("Samajh rhe ho..")
+}
+else if(name=="ashish"){
+    console.log("Bijli ka bill tera baap bharega..")
+}
+else if(name=="elvish"){
+    console.log("Sysssstummmm......")
+}
+else{
+    console.log("Murkh enterr correct name")
+}*/
+
+// Topic: Relational Operator
+
+/*let a=10;
+let b=20;
+
+let res1=a>b
+let res2=a<b
+let res3= a>=b
+let res4= a<=b
+
+let res5=(a==b)
+console.log(res5)*/
+
+
+/*let a=1;
+let b="1";
+
+console.log("Loose Equality ==",a==b)   
+console.log("Strict Equality ===",a===b)  
+
+console.log("Loose Not Equality != ",a!=b)  
+console.log("Strict Not Equality !== ",a!==b)  */
+
+
+/*let a=10;
+let b=20;
+let c=30;
+let d=40;
+
+
+let output= (a>b) && (c>d)
+            10>20 && 30>40
+console.log(output)  */
+
+
+// Logical OR ||
+
+/*const a=1;
+const b=2;
+const c=3;
+const d=4;
+
+const res= (a>b) || (c>d)
+        //  1>2 || 3>4
+console.log(res)*/
+              
+
+/* Topic: Conditional Operator
+
+let a=+prompt('Enter first number');
+let b=+prompt('Enter second number');
+
+const res=a>b?a:b;
+console.log("Biggest Number is: "+res)*/
+
+// const a=prompt('enter a number')
+// console.log(typeof(a))
+
+// // const b=Number(a)
+// console.log(typeof(b))
+
+
+
+// const a="10"
+// const b=+a
+// console.log(typeof(b))
