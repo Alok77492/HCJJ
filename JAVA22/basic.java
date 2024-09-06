a1d308f4c8a33d79f6baf4359612bbcfc3aca147
Alok Kumar Singh <alokks20031996@gmail.com>
2024-09-07 01:28:55 +0530

    learn

--- JAVA22/basic.java
+++ JAVA22/basic.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 class basic
 {
 	public static void main(String[] args)
 	{
-		System.out.println("this is my first program");
+		//System.out.println("this is my first program");
+		System.out.println("Enter the value of x:");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+
+		System.out.println("Enter the value of y:");
+		
+		int y = sc.nextInt();
+		int sum;
+		sum=x/y;
+		
+		System.out.println("the value of x:" +sum);
+		sc.close();
 	}
 }
